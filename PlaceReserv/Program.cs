f93b810342d6c9da62af2a7835f0275886c6c3a2
Sahand-Pc <Sahand-Pc@DESKTOP-51F1Q39>
2024-05-01 14:50:01 +0330

    update

--- PlaceReserv/Program.cs
+++ PlaceReserv/Program.cs
@@ -89,19 +89,12 @@ builder.Services.AddSwaggerGen(c =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// اضافه کردن تنظیمات برنامه
-
-
 
 var app = builder.Build();
 
 
 
 // Configure the HTTP request pipeline.
-
-
-
-
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseRouting();
